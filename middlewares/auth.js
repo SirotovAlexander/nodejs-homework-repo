743d375fd516c3364ad821c163be53102db7acbf
SirotovAlexander <alexander.sirotov@gmail.com>
2023-10-06 21:43:49 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const { SECRET_KEY } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     return res.status(401).json({
       status: "error",
       code: 401,
