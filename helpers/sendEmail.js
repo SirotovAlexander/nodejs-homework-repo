bfed18ca3d0cb8308a2c555658058d14d976e3c3
SirotovAlexander <alexander.sirotov@gmail.com>
2023-10-15 00:53:25 +0300

    add js code
    add send mail code

---
+++ helpers/sendEmail.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const { META_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "alexsirotov@meta.ua",
+    pass: META_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const email = {
+  to: "alexv1238@gmail.com",
+  from: "alexsirotov@meta.ua",
+  subject: "Test email",
+  html: "<p><strong>Test email</strong> from localhost:3000</p>",
+};
+
+transport
+  .sendMail(email)
+  .then(() => console.log("Email send success"))
+  .catch((error) => console.log(error.message));
